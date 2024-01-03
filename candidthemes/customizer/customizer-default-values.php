d696e6e939bdb6e22a058c5227a93b8c4396116d
Yos0107 <tamangyoseph@gmail.com>
2024-01-03 13:40:54 +0545

    changes

--- candidthemes/customizer/customizer-default-values.php
+++ candidthemes/customizer/customizer-default-values.php
@@ -9,7 +9,7 @@ if ( !function_exists('allure_news_default_theme_options_values') ) :
         $default_theme_options = array(
 
              /*General Colors*/
-            'allure-news-primary-color' => '#EB455F',
+            'allure-news-primary-color' => '#00ADB5',
             'allure-news-site-title-hover'=> '',
             'allure-news-site-tagline'=> '',
             
